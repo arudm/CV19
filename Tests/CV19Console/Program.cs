34f0d05b14338cbd78b6f3ceac2a1e902564c5f9
RusGameDraft <RusGameDraft@gmail.com>
2022-09-01 22:11:01 +0300

    HTTP запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        const string data_url = @"https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_string = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
         }
     }
 }
