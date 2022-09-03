2e30b26946ef4b8e56d495d6c3b2bc1a2148307a
RusGameDraft <RusGameDraft@gmail.com>
2022-09-03 14:54:36 +0300

    Дизайн главного окна для работы со студентами

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using CV19.Infrastructure.Commands;
 using CV19.Models;
+using CV19.Models.Decanat;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
-    {
+
+    {   
+        /*-------------------------------------------------------------------------------------------------*/
+        public ObservableCollection<Group> Groups { get; }
         /// <summary>Выбранный индекс вкладки</summary>
         #region Выбранный индекс вкладки
         private int _SelectedPageIndex = 0;
@@ -60,6 +66,8 @@ namespace CV19.ViewModels
 
         #endregion
 
+        /*-------------------------------------------------------------------------------------------------*/
+
         #region Команды
 
         #region CloseApplicationCommand
@@ -85,6 +93,9 @@ namespace CV19.ViewModels
 
         #endregion
         #endregion
+
+        /*-------------------------------------------------------------------------------------------------*/
+       
         public MainWindowViewModel()
         {
             #region Команды
@@ -104,6 +115,26 @@ namespace CV19.ViewModels
             }
 
             TestDataPoints = data_points;
+
+            var student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(i => new Student()
+            {
+                Name = $"Name {student_index}",
+                Surname=$"Surname {student_index}",
+                Patronymic=$"Patronymic {student_index++}",
+                Birthday=DateTime.Now,
+                Rating=0
+            });
+
+            var groups = Enumerable.Range(1, 20).Select(i => new Group()
+            {
+                Name = $"Группа {i}",
+                Students = new ObservableCollection<Student>(students)
+            });
+
+            Groups = new ObservableCollection<Group>(groups);
+
+
         }
     }
 }
