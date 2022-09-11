75a21126a3ae4d7eb0ae29e48945375718580f65
RusGameDraft <RusGameDraft@gmail.com>
2022-09-11 19:28:28 +0300

    Добавлены триггеры и анимации

--- CV19/Views/Windows/MainWindow.xaml.cs
+++ CV19/Views/Windows/MainWindow.xaml.cs
@@ -7,7 +7,7 @@ namespace CV19
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+       public MainWindow()
         {
             InitializeComponent();
         }
